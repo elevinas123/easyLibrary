9143e03a3b1f57f806b72f1c4166f72234d52114
elevinas123 <elvinas.monstvilas@gmail.com>
2024-08-17 16:42:17 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return <div className="bg-black text-gray-100 h-screen w-screen">Library</div>;
+}
+
+export default App;
