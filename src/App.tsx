4b86f4758518b1ec38c0716d4a7e8d9c3b1063a4
elevinas123 <elvinas.monstvilas@gmail.com>
2024-08-18 12:19:39 +0300

    making pdf extraction logic with py

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
     const [count, setCount] = useState(0);
+    useEffect(() => {
+        const loadingTask = pdfj.getDocument(url);
+        loadingTask.promise.then((pdf) => {
+            console.log("PDF loaded");
 
-    return <div className="bg-black text-gray-100 h-screen w-screen">Library</div>;
+            // Fetch the first page
+            pdf.getPage(1).then((page) => {
+                console.log("Page loaded");
+
+                page.getTextContent().then((textContent) => {
+                    // Process the text content
+                    textContent.items.forEach((item) => {
+                        console.log(item.str);
+                    });
+                });
+            });
+        });
+    }, [])
+    return (
+        <div className="bg-black text-gray-100 h-screen w-screen">Library</div>
+    );
 }
 
 export default App;
