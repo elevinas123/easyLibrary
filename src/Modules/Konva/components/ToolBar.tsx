509aff22ade6d1ce5a9b1c24a47d4b73a40dd451
elevinas123 <elvinas.monstvilas@gmail.com>
2024-08-28 23:02:27 +0300

    configured tools

--- src/Modules/Konva/components/ToolBar.tsx
+++ src/Modules/Konva/components/ToolBar.tsx
@@ -1,13 +1,5 @@
-import React, { useState } from "react";
-import {
-    FaUndo,
-    FaRedo,
-    FaTrash,
-    FaLink,
-    FaClone,
-    FaArrowUp,
-    FaArrowDown,
-} from "react-icons/fa";
+import React, { CSSProperties, useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
 const colors = [
     "#1e1e1e",
@@ -21,71 +13,97 @@ const colors = [
     "#ffec99",
 ];
 
+type ToolBarItemProps = {
+    item: string;
+    controlItem: string;
+    changeFunction: (item: string) => void;
+    style?: CSSProperties;
+    children?: React.ReactNode;
+};
+
+const ToolBarItem = ({
+    item,
+    controlItem,
+    changeFunction,
+    style,
+    children,
+}: ToolBarItemProps) => {
+    return (
+        <button
+            key={item}
+            className={`w-7 h-7 justify-center items-center flex rounded-lg border-2 bg-zinc-800 ${
+                controlItem === item
+                    ? "border-zinc-500 border bg-neutral-800"
+                    : "border-transparent"
+            }`}
+            style={style}
+            onClick={() => changeFunction(item)}
+        >
+            {children}
+        </button>
+    );
+};
+
 export default function ToolBar() {
     const [strokeColor, setStrokeColor] = useState("#f08c00");
     const [backgroundColor, setBackgroundColor] = useState("transparent");
     const [strokeWidth, setStrokeWidth] = useState("2.5");
     const [strokeStyle, setStrokeStyle] = useState("solid");
-    const [sloppiness, setSloppiness] = useState("architect");
     const [edges, setEdges] = useState("round");
     const [opacity, setOpacity] = useState(100);
 
     return (
-        <div className=" absolute left-0 z-50 top-36 flex flex-col space-y-4 p-4  bg-gray-900 rounded shadow-lg max-w-xs">
+        <div className="absolute left-0 z-50 top-36 flex flex-col space-y-4 p-4 bg-zinc-900 border border-zinc-500 rounded shadow-lg max-w-xs">
+            {/* Stroke Color */}
             <div>
                 <h3 className="text-sm font-semibold">Stroke</h3>
                 <div className="flex space-x-2 mt-2">
                     {colors.slice(0, 5).map((color) => (
-                        <button
+                        <ToolBarItem
                             key={color}
-                            className={`w-8 h-8 rounded-full border-2 ${
-                                strokeColor === color
-                                    ? "border-black"
-                                    : "border-transparent"
-                            }`}
+                            item={color}
+                            controlItem={strokeColor}
+                            changeFunction={setStrokeColor}
                             style={{ backgroundColor: color }}
-                            onClick={() => setStrokeColor(color)}
-                        ></button>
+                        />
                     ))}
                 </div>
             </div>
 
+            {/* Background Color */}
             <div>
-                <h3 className="text-sm font-semibold">Background</h3>
+                <h3 className="text-xs font-semibold">Background</h3>
                 <div className="flex space-x-2 mt-2">
                     {["transparent", ...colors.slice(5)].map((color) => (
-                        <button
+                        <ToolBarItem
                             key={color}
-                            className={`w-8 h-8 rounded-full border-2 ${
-                                backgroundColor === color
-                                    ? "border-black"
-                                    : "border-transparent"
-                            }`}
+                            item={color}
+                            controlItem={backgroundColor}
+                            changeFunction={setBackgroundColor}
                             style={{
                                 backgroundColor:
                                     color === "transparent" ? "white" : color,
                             }}
-                            onClick={() => setBackgroundColor(color)}
-                        ></button>
+                        />
                     ))}
                 </div>
             </div>
 
+            {/* Stroke Width */}
             <fieldset>
-                <legend className="text-sm font-semibold">Stroke width</legend>
+                <legend className="text-xs font-semibold">Stroke width</legend>
                 <div className="flex space-x-4 mt-2">
                     {[
                         { title: "Thin", value: "1.25" },
                         { title: "Bold", value: "2.5" },
                         { title: "Extra bold", value: "3.75" },
                     ].map(({ title, value }) => (
-                        <label key={value} title={title}>
-                            <input
-                                type="radio"
-                                name="stroke-width"
-                                checked={strokeWidth === value}
-                                onChange={() => setStrokeWidth(value)}
-                            />
+                        <ToolBarItem
+                            key={value}
+                            item={value}
+                            controlItem={strokeWidth}
+                            changeFunction={setStrokeWidth}
+                        >
                             <svg
                                 aria-hidden="true"
                                 focusable="false"
@@ -99,26 +117,26 @@ export default function ToolBar() {
                             >
                                 <path d="M5 10h10" strokeWidth={value} />
                             </svg>
-                        </label>
+                        </ToolBarItem>
                     ))}
                 </div>
             </fieldset>
 
+            {/* Stroke Style */}
             <fieldset>
-                <legend className="text-sm font-semibold">Stroke style</legend>
+                <legend className="text-xs font-semibold">Stroke style</legend>
                 <div className="flex space-x-4 mt-2">
                     {[
                         { title: "Solid", value: "solid" },
                         { title: "Dashed", value: "dashed" },
                         { title: "Dotted", value: "dotted" },
                     ].map(({ title, value }) => (
-                        <label key={value} title={title}>
-                            <input
-                                type="radio"
-                                name="strokeStyle"
-                                checked={strokeStyle === value}
-                                onChange={() => setStrokeStyle(value)}
-                            />
+                        <ToolBarItem
+                            key={value}
+                            item={value}
+                            controlItem={strokeStyle}
+                            changeFunction={setStrokeStyle}
+                        >
                             <svg
                                 aria-hidden="true"
                                 focusable="false"
@@ -148,64 +166,22 @@ export default function ToolBar() {
                                     </>
                                 )}
                             </svg>
-                        </label>
+                        </ToolBarItem>
                     ))}
                 </div>
             </fieldset>
 
+            {/* Edges */}
             <fieldset>
-                <legend className="text-sm font-semibold">Sloppiness</legend>
-                <div className="flex space-x-4 mt-2">
-                    {["architect", "artist", "cartoonist"].map((value) => (
-                        <label
-                            key={value}
-                            title={
-                                value.charAt(0).toUpperCase() + value.slice(1)
-                            }
-                        >
-                            <input
-                                type="radio"
-                                name="sloppiness"
-                                checked={sloppiness === value}
-                                onChange={() => setSloppiness(value)}
-                            />
-                            <svg
-                                aria-hidden="true"
-                                focusable="false"
-                                role="img"
-                                viewBox="0 0 20 20"
-                                className="w-5 h-5"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            >
-                                <path
-                                    d="M2.5 12.5c1.5-0.5 5-3 7.5-4s0 2 1 2 5-2 5-2"
-                                    strokeWidth="1.25"
-                                />
-                            </svg>
-                        </label>
-                    ))}
-                </div>
-            </fieldset>
-
-            <fieldset>
-                <legend className="text-sm font-semibold">Edges</legend>
+                <legend className="text-xs font-semibold">Edges</legend>
                 <div className="flex space-x-4 mt-2">
                     {["sharp", "round"].map((value) => (
-                        <label
+                        <ToolBarItem
                             key={value}
-                            title={
-                                value.charAt(0).toUpperCase() + value.slice(1)
-                            }
+                            item={value}
+                            controlItem={edges}
+                            changeFunction={setEdges}
                         >
-                            <input
-                                type="radio"
-                                name="edges"
-                                checked={edges === value}
-                                onChange={() => setEdges(value)}
-                            />
                             <svg
                                 aria-hidden="true"
                                 focusable="false"
@@ -224,13 +200,14 @@ export default function ToolBar() {
                                     <path d="M5 5h10v10H5z" rx="2" />
                                 )}
                             </svg>
-                        </label>
+                        </ToolBarItem>
                     ))}
                 </div>
             </fieldset>
 
-            <label className="control-label flex items-center space-x-2">
-                <span>Opacity</span>
+            {/* Opacity */}
+            <label className="control-label flex flex-col space-x-2">
+                <span className="text-xs ">Opacity</span>
                 <input
                     type="range"
                     min="0"
@@ -241,51 +218,18 @@ export default function ToolBar() {
                 />
             </label>
 
-            <fieldset>
-                <legend className="text-sm font-semibold">Layers</legend>
-                <div className="flex space-x-2 mt-2">
-                    <button
-                        type="button"
-                        className="zIndexButton"
-                        title="Send to back"
-                    >
-                        <FaArrowDown className="transform rotate-180" />
-                    </button>
-                    <button
-                        type="button"
-                        className="zIndexButton"
-                        title="Send backward"
-                    >
-                        <FaArrowDown />
-                    </button>
-                    <button
-                        type="button"
-                        className="zIndexButton"
-                        title="Bring forward"
-                    >
-                        <FaArrowUp />
-                    </button>
-                    <button
-                        type="button"
-                        className="zIndexButton"
-                        title="Bring to front"
-                    >
-                        <FaArrowUp className="transform rotate-180" />
-                    </button>
-                </div>
-            </fieldset>
+            {/* Layers */}
 
+            {/* Actions */}
             <fieldset>
                 <legend className="text-sm font-semibold">Actions</legend>
                 <div className="flex space-x-2 mt-2">
-                    <button type="button" title="Duplicate">
-                        <FaClone />
-                    </button>
-                    <button type="button" title="Delete">
+                    <button
+                        className={`w-7 h-7  p-1.5 justify-center items-center flex rounded-lg bg-zinc-700 
+                           
+                        `}
+                    >
                         <FaTrash />
-                    </button>
-                    <button type="button" title="Link">
-                        <FaLink />
                     </button>
                 </div>
             </fieldset>
