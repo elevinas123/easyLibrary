9bdb8cc5aba12b3cc61a877661d2fb210dce8f2c
elevinas123 <elvinas.monstvilas@gmail.com>
2024-09-16 18:51:37 +0300

    added nest js backend

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import MainPage from "./Modules/BookPage/MainPage";
-
-function App() {
-    return <MainPage />;
-}
-
-export default App;
--- src/atoms.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from "jotai";
-import { HighlightRange } from "./preprocess/epub/preprocessEpub";
-
-export const highlightedRangeAtom = atom<HighlightRange[]>([]);
