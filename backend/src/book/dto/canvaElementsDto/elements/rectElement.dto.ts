0a3531e326f826ea0219c48856db890aa0831279
elevinas123 <elvinas.monstvilas@gmail.com>
2024-09-25 15:24:15 +0300

    fixed a bug where rects couldnt move

--- backend/src/book/dto/canvaElementsDto/elements/rectElement.dto.ts
+++ backend/src/book/dto/canvaElementsDto/elements/rectElement.dto.ts
@@ -1,9 +1,18 @@
 // src/books/dto/rect-element.dto.ts
 
-import {IsEnum, IsString} from 'class-validator';
-import { CanvaElementSkeletonDto } from '../canvaElementSkeleton.dto';
-
+import { IsEnum, IsNumber, IsString } from "class-validator";
+import { CanvaElementSkeletonDto } from "../canvaElementSkeleton.dto";
 
 export class RectElementDto extends CanvaElementSkeletonDto {
-  @IsString() @IsEnum(['rect']) type: 'rect';
+    @IsString() @IsEnum(["rect"]) type: "rect";
+
+    @IsNumber() roughness: number;
+
+    @IsNumber() seed: number;
+
+    @IsString() fillStyle: string;
+
+    @IsNumber() hachureGap: number;
+
+    @IsNumber() hachureAngle: number;
 }
--- backend/src/book/schema/canvaElements/elements/rectElement.schema.ts
+++ backend/src/book/schema/canvaElements/elements/rectElement.schema.ts
@@ -1,14 +1,21 @@
 // rect-element.schema.ts
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
-import {
-    CanvaElementSkeleton,
-    CanvaElementSkeletonType,
-} from "../canvaElementSkeleton";
+import { CanvaElementSkeletonType } from "../canvaElementSkeleton";
 
 export interface RectElementType extends CanvaElementSkeletonType {}
-@Schema({_id: false})
-export class RectElement {}
+@Schema({ _id: false })
+export class RectElement {
+    @Prop({ required: true }) fillStyle: string;
+
+    @Prop({ required: true }) roughness: number;
+
+    @Prop({ required: true }) seed: number;
+
+    @Prop({ required: true }) hachureGap: number;
+
+    @Prop({ required: true }) hachureAngle: number;
+}
 
 export type RectElementDocument = RectElement & Document;
 export const RectElementSchema = SchemaFactory.createForClass(RectElement);
