6b38d236827f099c8d9ae91d2e8f88591276439a
elevinas123 <elvinas.monstvilas@gmail.com>
2025-01-05 00:29:06 +0200

    add keys and removed node modolues

--- frontend/src/Modules/LibraryPage/LibraryPage.tsx
+++ frontend/src/Modules/LibraryPage/LibraryPage.tsx
@@ -153,6 +153,7 @@ export default function LibraryPage() {
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
                     {bookData.map((book) => (
                         <BookCard
+                            key={book.id}
                             deleteBook={deleteBook}
                             book={book}
                             selectBook={selectBook}
