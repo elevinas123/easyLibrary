b69ad6e27ad4ff3cd9ad18de1d727e1e6cf8c440
elevinas123 <elvinas.monstvilas@gmail.com>
2025-01-04 14:57:53 +0200

    removed backend plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,9 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
-import backendPlugin from "./backendPlugin";
 
 export default defineConfig({
-    plugins: [backendPlugin(), react()],
+    plugins: [react()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
